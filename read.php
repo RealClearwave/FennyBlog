b88162f7dac1e8b90471241e82f0a413fd97f26a
Fedoraer <smduck@163.com>
2018-12-23 12:21:36 +0800

    Add files via upload

--- read.php
+++ read.php
@@ -1,10 +1,17 @@
 <?php
 $pas_name = "pages\\" . $_GET["page"] . ".txt";
 //echo $pas_name;
+echo "<html>";
+echo "<head>";
+echo "<title>" . $pas_name . "</title>";
+echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">";
+echo "</head>";
+echo "<body>";
 $file=fopen($pas_name,"r") or exit("Bad File!" . $pas_name);
 while(!feof($file))
 {
     echo fgets($file). "<br>";
 }
 fclose($file);
+echo "</body>";
 ?>
