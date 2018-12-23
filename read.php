93a328bbb7036ee8c413bdce587a883d29de4a24
Fedoraer <smduck@163.com>
2018-12-23 11:58:21 +0800

    Add files via upload

---
+++ read.php
@@ -0,0 +1,10 @@
+<?php
+$pas_name = "pages\\" . $_GET["test"] . ".txt";
+//echo $pas_name;
+$file=fopen($pas_name,"r") or exit("Bad File!" + $pas_name);
+while(!feof($file))
+{
+    echo fgets($file). "<br>";
+}
+fclose($file);
+?>
